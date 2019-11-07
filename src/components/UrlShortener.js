119b4fd1527e9e51ed692dc3d44d55d50007b351
Han <allen0346@gmail.com>
2019-11-07 09:47:40 +0800

    Adjust: transfer UrlShortener from plain javascript to react-hook funcion component

---
+++ src/components/UrlShortener.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import styles from './UrlShortener.scss';
+import config from '../../config/config';
+
+const apiDev = 'http://localhost:8080';
+const apiProduct = 'https://url-shortener-back.appspot.com';
+const apiOrigin = config.nodeEnvIsProd ? apiProduct : apiDev;
+
+
+
+
+const UrlShortener = () => {
+  const [url, setUrl] = useState('');
+  const onInputChange = (e) => {
+    setUrl(e.target.value);
+  }
+  const submitHandler = async () => {
+    const originalUrl = url;
+    const postData = async (obj) => {
+      const response = await fetch(`${apiOrigin}/api/shorturl/new`, {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'omit',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        redirect: 'follow',
+        referrer: 'no-referrer',
+        body: JSON.stringify(obj),
+      });
+      const data = await response.json();
+      return data;
+    };
+    const data = await postData({ originalUrl });
+    console.log(data);
+  }
+  return (
+    <div className={styles.container}>
+      <h1>url shortener</h1>
+      <div className={styles.inputPanel}>
+        <input
+          type="text"
+          className={styles.input}
+          onChange={onInputChange}
+        />
+        <input
+          type="submit"
+          value="Submit"
+          className={styles.button}
+          onClick={submitHandler}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default UrlShortener;
