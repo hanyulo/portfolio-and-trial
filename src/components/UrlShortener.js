923013e3f9f86b283fbca9d2dc1b59c21fafab39
Han <allen0346@gmail.com>
2019-11-14 14:33:35 +0800

    Adjust: 1. add response UI 2. add data-testid for testing-library/react

--- src/components/UrlShortener.js
+++ src/components/UrlShortener.js
@@ -1,38 +1,46 @@
 import React, { useState } from 'react';
 import styles from './UrlShortener.scss';
-import config from '../../config/config';
-
-const apiDev = 'http://localhost:8080';
-const apiProduct = 'https://url-shortener-back.appspot.com';
-const apiOrigin = config.nodeEnvIsProd ? apiProduct : apiDev;
+import { createShortUrl } from '../utils/api';
 
 
 const UrlShortener = () => {
+  const [errorState, setError] = useState(false);
+  const [shortenUrl, setShortenUrl] = useState(undefined);
   const [url, setUrl] = useState('');
   const onInputChange = (e) => {
     setUrl(e.target.value);
   };
   const submitHandler = async () => {
     const originalUrl = url;
-    const postData = async (obj) => {
-      const response = await fetch(`${apiOrigin}/api/shorturl/new`, {
-        method: 'POST',
-        mode: 'cors',
-        cache: 'no-cache',
-        credentials: 'omit',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        redirect: 'follow',
-        referrer: 'no-referrer',
-        body: JSON.stringify(obj),
-      });
-      const data = await response.json();
-      return data;
-    };
-    const data = await postData({ originalUrl });
-    console.log(data);
+    const result = await createShortUrl({ originalUrl });
+    if (result.error) {
+      setError(true);
+    } else {
+      setShortenUrl(result.data.shortenUrl);
+    }
   };
+
+  const Error = errorState && (
+    <div
+      className={`${styles.error} ${styles.response}`}
+      data-testid="urlShortener-error"
+    >
+      There is something wrong with the system
+    </div>
+  );
+
+  const ResultNote = shortenUrl && (
+    <div className={`${styles.result} ${styles.response}`}>
+      <span>shorten url: </span>
+      <div
+        className={styles.shortenUrl}
+        data-testid="urlShortener-result"
+      >
+        {shortenUrl}
+      </div>
+    </div>
+  );
+
   return (
     <div className={styles.container}>
       <h1>url shortener</h1>
@@ -41,14 +49,18 @@ const UrlShortener = () => {
           type="text"
           className={styles.input}
           onChange={onInputChange}
+          placeholder="original URL"
         />
         <input
           type="submit"
           value="Submit"
           className={styles.button}
           onClick={submitHandler}
+          data-testid="urlShortener-button"
         />
       </div>
+      {Error}
+      {ResultNote}
     </div>
   );
 };
