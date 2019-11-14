87578c83f994e488f7cf7629ed755a217049f1a8
Han <allen0346@gmail.com>
2019-11-14 14:30:05 +0800

    Adjust: change api port from 3000 to 8080 in dev mode

--- config/config.js
+++ config/config.js
@@ -4,5 +4,5 @@ const nodeEnvIsProd = process.env.NODE_ENV === env.production;
 export default Object.freeze({
   nodeENV: process.env.NODE_ENV === env.production ? env.production : env.development,
   nodeEnvIsProd,
-  apiOrigin: nodeEnvIsProd ? 'https://url-shortener-back.appspot.com' : 'http://localhost:3000',
+  apiOrigin: nodeEnvIsProd ? 'https://url-shortener-back.appspot.com' : 'http://localhost:8080',
 });
