7a65c7b9c7361ae080892b5073bda2d4a0d82942
Han <allen0346@gmail.com>
2019-11-27 15:21:05 +0800

    Adjust: use specific renderer based on working env

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import StyleContext from 'isomorphic-style-loader/StyleContext';
 import App from './app';
+import config from '../config/config';
 
 const insertCss = (...styles) => {
   const removeCss = styles.map(style => style._insertCss())
   return () => removeCss.forEach(dispose => dispose())
 };
 
-ReactDOM.hydrate(
+const renderer = config.nodeEnvIsProd ? ReactDOM.hydrate : ReactDOM.render;
+
+renderer(
   <BrowserRouter>
     <StyleContext.Provider value={{ insertCss }}>
       <App />
