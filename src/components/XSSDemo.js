35de53b28a53641aa205e20ca3f318f151ba35c5
Han <allen0346@gmail.com>
2019-11-28 10:49:40 +0800

    Adjust: finish XSSDemo with three subTypes 1. Stored 2. Reflected 3. DOMBased

--- src/components/XSSDemo.js
+++ src/components/XSSDemo.js
@@ -15,11 +15,14 @@ class XSSDemo extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      list: [],
+      comment: '',
     };
-    this.inputNode = null;
+    this.inputNodeDOM = null;
+    this.commentNode = null;
+    this.tmpListContainer = null;
     this.redirectWithMaliciousQuery = this._redirectWithMaliciousQuery.bind(this);
     this.createLink = this._createLink.bind(this);
+    this.createList = this._createList.bind(this);
   }
 
   _redirectWithMaliciousQuery() {
@@ -28,11 +31,23 @@ class XSSDemo extends Component {
   }
 
   _createLink() {
-    const { value } = this.inputNode;
+    const { value } = this.commentNode;
     this.setState({
-      list: [...this.state.list, value],
+      comment: value,
     });
-    this.inputNode.value = '';
+    this.commentNode.value = '';
+  }
+
+  _createList() {
+    if (this.tmpListContainer && this.inputNodeDOM) {
+      const { value } = this.inputNodeDOM;
+      const divElement = document.createElement('div');
+      divElement.innerHTML = value;
+      const listElement = document.createElement('li');
+      listElement.appendChild(divElement);
+      this.tmpListContainer.appendChild(listElement);
+      this.inputNodeDOM.value = '';
+    }
   }
 
   _renderReflected() {
@@ -44,8 +59,12 @@ class XSSDemo extends Component {
       <div className={styles.section}>
         <h2>Reflected XSS</h2>
         <div>
+          <div className={styles.indicator}>Concept</div>
+          <div>Malicious script should be part of request payload. It will flow to application through server response.</div>
+        </div>
+        <div>
           <div className={styles.indicator}>Overview</div>
-          <div>Say, hacker tricks you to click on an Malicious Image through social engineering. Then you browse the website with following query string</div>
+          <div>Say, hacker tricks you to click on an Malicious Image through social engineering. Then you browse the website with following query. The malicious script will be injected into app thorugh query string</div>
         </div>
         <div>
           <div className={styles.indicator}>Malicious Query String: </div>
@@ -77,55 +96,64 @@ class XSSDemo extends Component {
   }
 
   _renderStored() {
-    const { list } = this.state;
+    const { comment } = this.state;
 
-    const ListOfLink = list.map((link, index) => {
-      return (
-        <li>
-          <a href={link}>
-            {link}
-          </a>
-        </li>
-      );
-    });
+    // const ListOfLink = list.map((link, index) => {
+    //   return (
+    //     <li>
+    //       <a href={link}>
+    //         {link}
+    //       </a>
+    //     </li>
+    //   );
+    // });
+    const Comment = <div className={styles.comment} dangerouslySetInnerHTML={{__html: comment}} />
 
     return (
       <div className={styles.section}>
         <h2>Stored XSS</h2>
         <div>
-          <div className={styles.indicator}>Overview</div>
-          <div>In this example, I only store links in react state obejct, which is a way to skip the back-end API implementation. To fullfill the Stored XSS requirement, I should store those data in database and request the page again.</div>
+          <div className={styles.indicator}>Concept</div>
+          <div>Inject malicious script into database</div>
         </div>
         <div>
-          <div className={styles.indicator}>List of Link: </div>
-          <ul>
-            {ListOfLink}
-          </ul>
+          <div className={styles.indicator}>Overview</div>
+          <div>
+            In this example, I only store comment in react state obejct, which is a way to skip the back-end API implementation. To fullfill the Stored XSS requirement, I should store those data in database and request the page again.
+            <br/>
+            The horrendous consquence is that if the malicious script is injected into database, every user who request the comments from databse will be hacked.
+          </div>
         </div>
         <div>
           <div className={styles.indicator}>
             Malicious Input Value
           </div>
           <div className={styles.dataBox}>
-            {`javascript:document.cookie="sessionId=DF2dfr34GG5ds246"; alert(document.cookie);`}
+            {`<a href="javascript:document.cookie=&quot;sessionId=DF2dfr34GG5ds246&quot;; alert(document.cookie);">thie is malicious link</a>`}
           </div>
         </div>
         <div>
           <div className={styles.indicator}>Vulnerability: </div>
           <div className={styles.dataBox}>
-            {`<a href={javascript:alert('hack!!!!')}>xyz</a>`}
+            {`dangerouslySetInnerHTML`}
           </div>
         </div>
         <div>
+          <div className={styles.indicator}>Comment: </div>
+          <ul>
+            {Comment}
+          </ul>
+        </div>
+        <div>
           <div className={styles.indicator}>
-            Type whatever you want to create a link then click on the link
+            Type whatever you want to create a comment
           </div>
           <div className={styles.input}>
             <input
               type="text"
               ref={(node) => {
-                if (!this.inputNode) {
-                  this.inputNode = node;
+                if (!this.commentNode) {
+                  this.commentNode = node;
                 }
               }}
             />
@@ -144,12 +172,74 @@ class XSSDemo extends Component {
     );
   }
 
+  _renderDOMBased() {
+    return (
+      <div className={styles.section}>
+        <h2>DOMBased</h2>
+        <div>
+          <div className={styles.indicator}>Concept</div>
+          <div>Malicious script is injected through DOM manipulation</div>
+        </div>
+        <div>
+          <div className={styles.indicator}>Overview</div>
+          <div>N/A</div>
+        </div>
+        <div>
+          <div className={styles.indicator}>Malicious Script</div>
+          <div className={styles.dataBox}>{`<a href="javascript:document.cookie=&quot;sessionId=DF2dfr34GG5ds246&quot;; alert(document.cookie);">xyz</a>`}</div>
+        </div>
+        <div>
+          <div className={styles.indicator}>Vulnerability</div>
+          <ul>
+            <li><div className={styles.dataBox}>{`document.appendChild`}</div></li>
+            <li><div className={styles.dataBox}>{`document.createElement('div').innerHTML = maclicious script`}</div></li>
+          </ul>
+        </div>
+        <div>
+          <div className={styles.indicator}>Todo List:</div>
+          <ul
+            ref={(node) => {
+              if (!this.tmpListContainer) {
+                this.tmpListContainer = node;
+              }
+            }}
+          />
+        </div>
+        <div>
+          <div className={styles.indicator}>
+            Type whatever you want to create a Todo list
+          </div>
+          <div className={styles.input}>
+            <input
+              type="text"
+              ref={(node) => {
+                if (!this.inputNodeDOM) {
+                  this.inputNodeDOM = node;
+                }
+              }}
+            />
+          </div>
+          <div
+            className={styles.button}
+            onClick={this.createList}
+            onKeyDown={() => {}}
+            role="button"
+            tabIndex={0}
+          >
+            Create
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className={styles.container}>
         <h1>XSS Demo</h1>
-        {this._renderReflected()}
         {this._renderStored()}
+        {this._renderReflected()}
+        {this._renderDOMBased()}
         <div>Icons made by <a href="https://www.flaticon.com/authors/vitaly-gorbachev" title="Vitaly Gorbachev">Vitaly Gorbachev</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
       </div>
     );
