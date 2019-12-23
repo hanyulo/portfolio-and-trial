4fa5805b8ed86a06351530ffd71cf73ed3648636
Han <allen0346@gmail.com>
2019-12-23 09:44:48 +0800

    Adjust: add apiHostName to config

--- config/config.js
+++ config/config.js
@@ -4,5 +4,6 @@ const nodeEnvIsProd = process.env.NODE_ENV === env.production;
 export default Object.freeze({
   nodeENV: process.env.NODE_ENV === env.production ? env.production : env.development,
   nodeEnvIsProd,
-  apiOrigin: nodeEnvIsProd ? 'https://url-shortener-back.appspot.com' : 'http://localhost:8080',
+  apiOrigin: process.env.NODE_ENV === env.production ? 'https://url-shortener-back.appspot.com' : 'http://localhost:3033',
+  apiHostName: process.env.NODE_ENV === env.production ? 'url-shortener-back.appspot.com' : 'localhost:3033',
 });
