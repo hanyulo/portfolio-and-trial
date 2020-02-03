d4b9a17c46d59afb4bd5ce35ee80c8648df9fa71
Han <allen0346@gmail.com>
2020-02-03 13:55:01 +0800

    Adjust: add global style of styled-components

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
 import {
   Switch,
   Route,
@@ -39,42 +40,69 @@ function PrivateRoute({ key, path, component: Component, authorization }) {
   );
 }
 
+const theme = {
+  navBarBackgroundColor: '#242831',
+  dark: '#333740',
+  light: '#FBFBFB',
+  h1Color: '#F8DA35',
+  masonryHeader: '#efd254',
+  fontFamily: '"Times New Roman", Times, serif',
+};
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: ${props => props.location.pathname === '/' ? props.theme.dark : props.theme.light};
+    color: ${props => props.location.pathname === '/' ? 'white' : 'black'};
+    font-family: ${props => props.theme.fontFamily};
+    input {
+      background-color: ${props => props.theme.light};
+    }
+  }
+`;
+
 // TODO: userProfile is the props that should be passed by the server-side (not yet impleneted)
 const App = ({ userProfile }) => {
   return (
-    <Route
-      render={({ location, history }) => {
-        return (
-          <AppShell
-            location={location}
-            history={history}
-            userProfile={userProfile}
-          >
-            <Switch>
-              {
-                routes.map((route, routeIndex) => {
-                  // TODO: need to be used in SSR
-                  // if (route.authorization) {
-                  //   return (
-                  //     <PrivateRoute
-                  //       key={`react-route-${routeIndex}`}
-                  //       {...route}
-                  //     />
-                  //   );
-                  // }
-                  return (
-                    <Route
-                      key={`react-route-${routeIndex}`}
-                      {...route}
-                    />
-                  );
-                })
-              }
-            </Switch>
-          </AppShell>
-        );
-      }}
-    />
+      <Route
+        render={({ location, history }) => {
+          return (
+            <ThemeProvider
+              theme={theme}
+            >
+              <AppShell
+                location={location}
+                history={history}
+                userProfile={userProfile}
+              >
+                <Switch>
+                  {
+                    routes.map((route, routeIndex) => {
+                      // TODO: need to be used in SSR
+                      // if (route.authorization) {
+                      //   return (
+                      //     <PrivateRoute
+                      //       key={`react-route-${routeIndex}`}
+                      //       {...route}
+                      //     />
+                      //   );
+                      // }
+                      return (
+                        <Route
+                          key={`react-route-${routeIndex}`}
+                          {...route}
+                        />
+                      );
+                    })
+                  }
+                </Switch>
+              </AppShell>
+              <GlobalStyle
+                location={location}
+              />
+            </ThemeProvider>
+          );
+        }}
+      />
   );
 }
 
